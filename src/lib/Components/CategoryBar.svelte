a65fa7cd3da4240f404cb3cb608dbfd9b0a6ac55
Osokin Yaroslav <osyaros.2005@yandex.ru>
2024-04-10 17:54:53 +0300

    sync 1

---
+++ src/lib/Components/CategoryBar.svelte
@@ -0,0 +1,28 @@
+<script>
+   
+</script>
+
+<style>
+    .categorybar{
+        display: flex;
+        background-color: #1C1C1C;
+        flex-direction: column;
+    }
+</style>
+
+<div class="categorybar">
+    <ul>
+        <li>
+            Все
+        </li>
+        <li>
+            Учеба
+        </li>
+        <li>
+            
+        </li>
+        <li>
+            
+        </li>
+    </ul>
+</div>
